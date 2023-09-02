7539caf52de0f855158e27add50a716666dce43e
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2023-09-01 21:15:52 -0300

    Lista de Gerações e Botão de voltar ao topo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
+const scrollTopButton = document.getElementById('returnToTopButton');
 const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
-const limit = 5
+// const loadMoreButton = document.getElementById('loadMoreButton')
+const generationSelector = document.getElementById('generation')
+let limit = 151
 let offset = 0
 
 function convertPokemonTypesToLi(pokemonTypes) {
@@ -8,21 +10,45 @@ function convertPokemonTypesToLi(pokemonTypes) {
 }
 
 function loadPokemonItems(offset, limit) {
-  
 
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    pokemonList.classList.remove("loading")
+    pokemonList.classList.add("pokemons")
+    pokemonList.innerHTML = ''
     pokemonList.innerHTML += pokemons.map(pokemon => {
       var pokemonName
+
+      // switch(pokemonName) {
+      //   case 'nidoran-m':
+      //     pokemonName = 'nidoran ♂'
+      //   case 'nidoran-f':
+      //     pokemonName = 'nidoran ♀'
+      //   case 'farfetchd':
+      //     pokemonName = `farfetch'd`
+      //   case 'giratina-altered':
+      //     pokemonName = 'giratina'
+      //   case 'deoxys-normal':
+      //     pokemonName = 'deoxys'
+      // }
+
       if(pokemon.name === 'nidoran-m') {
         pokemonName = 'nidoran ♂'
       } else if(pokemon.name === 'nidoran-f') {
         pokemonName = 'nidoran ♀'
       } else if(pokemon.name === 'farfetchd') {
         pokemonName = `farfetch'd`
-      }else {
+      } else if(pokemon.name === 'deoxys-normal') {
+        pokemonName = `deoxys`
+      } else if(pokemon.name === 'giratina-altered') {
+        pokemonName = `giratina`
+      } else if(pokemon.name === 'shaymin-land') {
+        pokemonName = `shaymin`
+      } else if(pokemon.name === 'wormadam-plant') {
+        pokemonName = `wormadam`
+      } else {
         pokemonName = pokemon.name
       }
-      
+
       return `
         <li class="pokemon ${pokemon.type}">
           <span class="number">#${("000" + pokemon.id).slice(-3)}</span>
@@ -39,9 +65,41 @@ function loadPokemonItems(offset, limit) {
   })
 }
 
+pokemonList.classList.remove("pokemons")
+pokemonList.classList.add("loading")
+
+pokemonList.innerHTML = '<img class="loading__img" src="https://icon-library.com/images/pokeball-icon-png/pokeball-icon-png-19.jpg" alt="">'
 loadPokemonItems(offset, limit)
 
-loadMoreButton.addEventListener('click', () => {
-  offset += limit
-  loadPokemonItems(offset, limit)
+// loadMoreButton.addEventListener('click', () => {
+//   offset += limit
+//   loadPokemonItems(offset, limit)
+// })
+
+generationSelector.addEventListener('change', () => {
+  pokemonList.classList.remove("pokemons")
+  pokemonList.classList.add("loading")
+  pokemonList.innerHTML = '<img class="loading__img" src="https://icon-library.com/images/pokeball-icon-png/pokeball-icon-png-19.jpg" alt="">'
+  
+  const generationSelected = parseInt(generationSelector.value)
+
+  console.log(generationSelected)
+
+  loadPokemonItems(offset, generationSelected)
+  
+  // switch(generationSelected){
+  //   case '151':
+  //     loadPokemonItems(offset, 151);
+  //   case '251':
+  //     loadPokemonItems(offset, 251);
+  //}
+
 })
+
+
+scrollTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+});
